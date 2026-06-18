bebcd7913f1d45dfa8835b76843be82ba7848460
jata4649 <jatarou56@gmail.com>
2026-06-18 14:52:43 +0900

    Update topics.js

--- topics.js
+++ topics.js
@@ -1,1 +1,13 @@
+const TOPICS = [
+  { majority: "猫", minority: "虎" },
+  { majority: "犬", minority: "狼" },
+  { majority: "りんご", minority: "トマト" },
+  { majority: "車", minority: "バス" },
+  { majority: "電車", minority: "新幹線" },
+  { majority: "魚", minority: "イルカ" },
+  { majority: "鳥", minority: "ペンギン" },
+  { majority: "傘", minority: "きのこ" },
+  { majority: "魔法使い", minority: "魔女" },
+  { majority: "王様", minority: "サンタ" }
+];
 
